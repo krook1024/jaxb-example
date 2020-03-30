ba815a26a6047438769e93d60fb265634c2735cb
krook1024 <krook1024@gmail.com>
2020-03-30 22:04:37 +0200

    Created LegoSet data structure

---
+++ src/main/java/lego/LegoSet.java
@@ -0,0 +1,38 @@
+package lego;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.net.URL;
+import java.time.Year;
+import java.util.List;
+import java.util.Set;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
+public class LegoSet {
+    private String name;
+    private String theme;
+    private String subtheme;
+
+    @XmlJavaTypeAdapter(YearAdapter.class)
+    private Year year;
+
+    private int pieces;
+
+    @XmlElementWrapper(name = "tags")
+    @XmlElement(name = "tag")
+    private Set<String> tags;
+
+    @XmlElementWrapper(name = "minifigs")
+    @XmlElement(name = "minifig")
+    private List<Minifig> minifigs;
+
+    private Weight weight;
+    private URL url;
+
+    @XmlAttribute
+    private String number;
+}
